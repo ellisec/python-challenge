70746fce132685022cd3c3e0ef9d18bd246eb77f
Ellise Carpenter <46426909+ellisec@users.noreply.github.com>
2019-03-01 22:55:02 -0600

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,21 @@
 # PyBank file
+
+import os
+import csv
+
+csvpath = "C:\\Users\\ellis\\Downloads\\budget.csv"
+
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    
+def Analysis(fin_data):
+    months = int(sum(fin_data[0]))
+    total_amount = int(sum(fin_data[1]))
+    mon_max = int(max(fin_data[1]))
+    mon_min = int(min(fin_data[1]))
+
+    print(f"Total Months: {str(months)}")
+    print(f"Total: {str(total_amount)}")
+    print(f"Greatest Increase in Profits: {str(mon_max)}")
+    print(f"Greatest Decrease in Profits: {str(mon_min)}")
